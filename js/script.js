90f9bf09e7434a28da852ce914e4b34e40a74a93
Pavel-Vladimirovich <nesoglasnyj.pavel@gmail.com>
2022-10-04 16:57:23 +0300

    верстка секции pricing 04.10

---
+++ js/script.js
@@ -0,0 +1,19 @@
+function togglePrice() {
+	const billing = document.querySelector('.billing');
+	const billingItems = document.querySelectorAll('.billing__item')
+
+	billing.addEventListener('click', () => {
+		billing.classList.toggle('active')
+
+		if (!billing.classList.contains('active')) {
+			billingItems[0].classList.add('active')
+			billingItems[2].classList.remove('active')
+		}
+		else {
+			billingItems[0].classList.remove('active')
+			billingItems[2].classList.add('active')
+		}
+	})
+}
+
+togglePrice();
