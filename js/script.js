189d75115aa292ce00a3448aa6bad977d80f0c0f
Pavel-Vladimirovich <nesoglasnyj.pavel@gmail.com>
2022-10-25 23:27:25 +0300

    интеграция меню бургера. Добавлен HTML код, неоходимо подправит css код. JS код работает исправно 25.10

--- js/script.js
+++ js/script.js
@@ -1,3 +1,43 @@
+// Menu Burger =========================================
+// меню бургер
+const iconMenu = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.menu__body');
+if (iconMenu) {
+    iconMenu.addEventListener("click", () => {
+        document.body.classList.toggle('_lock');
+        menuBody.classList.toggle('_active');
+        iconMenu.classList.toggle('_active');
+    });
+}
+
+// прокрутка при клике
+
+const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
+if (menuLinks.length > 0) {
+    menuLinks.forEach(menuLink => {
+        menuLink.addEventListener("click", onMenuLinkClick)
+    });
+}
+function onMenuLinkClick(e) {
+    const menuLink = e.target;
+    if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+        const gotoBlock = document.querySelector(menuLink.dataset.goto);
+        const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
+
+        if (iconMenu.classList.contains('_active')) {
+            document.body.classList.remove('_lock');
+            iconMenu.classList.remove('_active');
+            menuBody.classList.remove('_active');
+        }
+
+        window.scrollTo({
+            top: gotoBlockValue,
+            behavior: "smooth"
+        });
+        e.preventDefault();
+    }
+}
+//======================================================
 function togglePrice() {
 	const billing = document.querySelector('.billing');
 	const billingItems = document.querySelectorAll('.billing__item')
@@ -15,7 +55,6 @@ function togglePrice() {
 		}
 	})
 }
-
 togglePrice();
 
 let acc = document.getElementsByClassName("accordion");
